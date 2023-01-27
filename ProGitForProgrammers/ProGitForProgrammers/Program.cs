70a9b0bf3f6441a18f3810fdf14613d61268781d
Andy Chiou <schiou@outlook.com>
2023-01-27 15:32:26 +0800

    01/27 modified

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -12,6 +12,7 @@ namespace ProGitForProgrammers
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("I just added this in visual studio");
+            Console.WriteLine("This line add to show the staging area");
         }
     }
 }
